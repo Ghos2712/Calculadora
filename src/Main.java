f508cf309e450a1e7bdb30fd4cdc45ef52eeee08
wilfri <wilfribrazonr@gmail.com>
2025-02-23 15:57:50 +0100

    Calculadora 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,29 +7,81 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Introduceme si quieres restar o si quieres sumar");
+
+
+        String cal;
+        System.out.println(" Introduceme si quieres restar sumar multiplicar o dividir ");
         Scanner scanner = new Scanner(System.in);
-        String mesa = scanner.nextLine() ;
-        if (mesa.equals("sumar")){
-            System.out.println("Introduce el primer numero");
-            int numero1 = scanner.nextInt() ;
+        cal = scanner.nextLine();
+        while(!cal.equals("salir")) {
 
 
-            System.out.println("Introduce el segundo numero");
-            int numero2= scanner.nextInt() ;
-            int resultado= numero1+numero2 ;
 
-            System.out.println("La suma " + numero1+ " mas " + numero2+" es igual a "+ resultado) ;
-
-        } else if (mesa.equals("restar")) {
-            System.out.println("Inserte el primer numero");
-            int number1= scanner.nextInt() ;
-            System.out.println("inserte el segundo numero" );
-            int number2=scanner.nextInt() ;
-            int resultado= number1-number2 ;
-            System.out.println(" la resta "+number1+ " mas "+ number2+ " es igual a "+ resultado);
+            if (cal.equals("sumar")) {
+                System.out.println("empecemos la suma");
+                System.out.println("Introduce un numero");
+                int numero1 = scanner.nextInt();
 
 
-        }
+                System.out.println("Introduce un numero");
+                int numero2 = scanner.nextInt();
+                int resultado = numero1 + numero2;
+
+                System.out.println("La suma de " + numero1 + " mas " + numero2 + " es igual a " + resultado);
+
+            } else if (cal.equals("restar")) {
+                System.out.println("empecemos la resta");
+                System.out.println("Introduce un numero");
+                int number1 = scanner.nextInt();
+                System.out.println("introduce un numero");
+                int number2 = scanner.nextInt();
+                int resultado = number1 - number2;
+                System.out.println(" la resta de " + number1 + " menos " + number2 + " es igual a " + resultado);
+
+
+            } else if (cal.equals("multiplicar")) {
+                System.out.println("Empecemos la multiplicacion");
+                System.out.println("introduce un numero");
+                int numero3 = scanner.nextInt();
+                System.out.println("introduce un numero");
+                int numero4 = scanner.nextInt();
+                int respuesta = numero3 * numero4;
+                System.out.println(" la multiplicacion " + numero3 + " por " + numero4 + " es igual a " + respuesta);
+
+
+            } else if (cal.equals("dividir")) {
+
+                double numero6;
+                do {
+                    System.out.println("Empecemos la division");
+                    System.out.println("intriduce un numero ");
+                    double numero5 = scanner.nextInt();
+                    System.out.println("introduce un numero");
+                     numero6 = scanner.nextInt();
+                    if (numero6 == 0) {
+                        System.out.println("error, no se puede introducir 0 introduzca otro numero");
+
+                    }else{
+
+                        double respuesta = numero5 / numero6;
+                        System.out.println(" la division " + numero5 + " entre " + numero6 + " es igual a  " + respuesta);
+
+                        System.out.println("Gracias por su tiempo");
+                    }
+
+
+
+
+                } while (numero6 == 0);
+
+            }
+            System.out.println("Introduceme si quieres restar sumar multiplicar o dividir ");
+            cal = scanner.nextLine();
+            scanner.nextLine();
+        };
+
+
+
+
     }
 }
